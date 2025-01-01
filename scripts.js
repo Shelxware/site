aed10189f7bafac65a9059adc2fd9e91fea3eb0d
Shelxware <ajjjjjwgw18@gmail.com>
2025-01-01 03:32:49 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -90,7 +90,7 @@
             margin: 0;
             padding: 0;
             position: absolute;
-            top: 50%;
+            top: 50000%;
             left: 50%;
             transform: translate(-50%, -50%);
             z-index: 10;
